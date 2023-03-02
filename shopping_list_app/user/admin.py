e651535b5719d76956c7736bffbea638c5b2eb6e
goosLy1 <gray.goosly@gmail.com>
2023-03-03 02:40:34 +0300

    Add products model in admin panel

--- shopping_list_app/user/admin.py
+++ shopping_list_app/user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Shopping_list
+from .models import Shopping_list, Product
 
 
 admin.site.register(Shopping_list)
+admin.site.register(Product)
